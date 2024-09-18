5c9ffba4a1192baf562581b8124b4b55318548f1
dev-brunosantos <brunosflauta@gmail.com>
2024-09-18 15:40:04 -0300

    Criando rotas de Usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CargosControllers } from "./controllers/CargosControllers";
+import { UsuarioController } from "./controllers/UsuarioControllers";
 
 const routes = Router()
 
@@ -9,5 +10,13 @@ routes.get('/cargos', CargosControllers.listar)
 routes.get('/cargos/:id', CargosControllers.listarId)
 routes.put('/cargos/editar/:id', CargosControllers.editar)
 routes.delete('/cargos/apagar/:id', CargosControllers.apagar)
+
+// ROTAS DE USUÁRIOS 
+routes.post('/usuario/cadastrar', UsuarioController.criar)
+routes.get('/usuario', UsuarioController.listar)
+routes.get('/usuario/:id', UsuarioController.listarUsuarioId)
+routes.put('/usuario/editar/:id', UsuarioController.editar)
+routes.put('/usuario/editar/senha/:id', UsuarioController.editarSenha)
+routes.delete('/usuario/apagar/:id', UsuarioController.apagar)
 
 export { routes }
