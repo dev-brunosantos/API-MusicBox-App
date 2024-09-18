06459607f8ee50c0c27beb2ce3f76a367c5744f6
dev-brunosantos <brunosflauta@gmail.com>
2024-09-18 15:15:03 -0300

    Criando os serviços de Usuário

--- src/services/UsuarioServices.ts
+++ src/services/UsuarioServices.ts
@@ -29,6 +29,47 @@ class UsuarioServices {
         }
         return { listaUsuarios }
     }
+    async listarUsuarioID(id: string) {
+        const usuarioId = await usuario.findFirst({ where: { id } })
+        if (!usuarioId) {
+            return { erro: "Não existe usuário com o ID informado." }
+        }
+        return { usuarioId }
+    }
+    async editarUsuario(id: string, nome: string, email: string) {
+        const usuarioId = await usuario.findFirst({ where: { id } })
+        if (usuarioId) {
+            const editar = await usuario.update({
+                where: { id },
+                data: { nome, email }
+            })
+            return {
+                status: "Os dados do usuário foram editado com sucesso.",
+                dados_antigos: usuarioId,
+                dados_atualizados: editar
+            }
+        }
+        return { erro: "Nenhum usuário encontrado com o ID informado." }
+    }
+    async editarSenha(id: string, senha: string) {
+        const usuarioId = await usuario.findFirst({ where: { id } })
+        if (usuarioId) {
+            const editar = await usuario.update({
+                where: { id },
+                data: { senha }
+            })
+            return { status: "A senha do usuário foi editada com sucesso." }
+        }
+        return { erro: "Nenhum usuário encontrado com o ID informado." }
+    }
+    async apagarUsuario(id: string) {
+        const usuarioId = await usuario.findFirst({ where: { id } })
+        if(usuarioId) {
+            const apagar = await usuario.delete({ where: { id }})
+            return { status: `O usuário ${apagar.nome} foi exluído do sistema.`}
+        }
+        return { erro: "Nenhum usuário encontrado com o ID informado." }
+    }
 }
 
 export { UsuarioServices }
