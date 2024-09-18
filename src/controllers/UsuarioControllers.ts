8585f2a18cb54b1f2f8e85d77cd17f07d76c3144
dev-brunosantos <brunosflauta@gmail.com>
2024-09-18 15:22:49 -0300

    Criando os controllers do USUARIO

--- src/controllers/UsuarioControllers.ts
+++ src/controllers/UsuarioControllers.ts
@@ -3,22 +3,43 @@ import { hash } from 'bcrypt';
 import { UsuarioServices } from "../services/UsuarioServices";
 
 
-const services = new UsuarioServices() ;
+const services = new UsuarioServices();
 
 class Usuario {
     async criar(req: Request, res: Response) {
-        const { nome, email, senha, cargoId} = req.body 
+        const { nome, email, senha, cargoId } = req.body
         const senhaCriptografada = await hash(senha, 10)
-        const criar = await services.criarUsuario({nome, email, senhaCriptografada, cargoId})
+        const criar = await services.criarUsuario({ nome, email, senhaCriptografada, cargoId })
         return res.json(criar)
     }
     async listar(req: Request, res: Response) {
         const usuarios = await services.listarUsuarios()
         return res.json(usuarios)
     }
+    async listarUsuarioId(req: Request, res: Response) {
+        const { id } = req.params
+        const usuarioId = await services.listarUsuarioID(id)
+        return res.json(usuarioId)
+    }
+    async editar(req: Request, res: Response) {
+        const { id } = req.params
+        const { nome, email } = req.body
+        const editarUsuario = await services.editarUsuario(id, nome, email)
+        return res.json(editarUsuario)
+    }
+    async editarSenha(req: Request, res: Response) {
+        const { id } = req.params
+        const { senha } = req.body
+        const novaSenha = await services.editarSenha(id, senha)
+        return res.json(novaSenha)
+    }
+    async apagar(req: Request, res: Response) {
+        const { id } = req.params
+        const apagarUsuario = await services.apagarUsuario(id)
+        return res.json(apagarUsuario)
+    }
 }
 
 const UsuarioController = new Usuario()
-
 
 export { UsuarioController }
