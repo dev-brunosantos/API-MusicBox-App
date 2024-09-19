f8cb37b61d48963ae22e9efdd9f4d1d0fe95d9c5
dev-brunosantos <brunosflauta@gmail.com>
2024-09-19 16:11:32 -0300

    Criando o arquivo LoginControllers.ts

---
+++ src/controllers/LoginControllers.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { LoginServices } from "../services/LoginServices";
+
+const services = new LoginServices()
+
+class Login {
+    async signIn(req: Request, res: Response) {
+        const { email, senha } = req.body
+        const login = await services.signIn({email, senha})
+        return res.json(login)
+    }
+}
+
+const LoginControllers = new Login()
+
+export { LoginControllers }
