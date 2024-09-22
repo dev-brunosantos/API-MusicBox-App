b0d9399d4819546aff60612f74f1125219383e31
dev-brunosantos <brunosflauta@gmail.com>
2024-09-22 12:53:22 -0300

    Sobreescrevendo a tipagem do Express

--- src/middlewares/validarAutenticacao.ts
+++ src/middlewares/validarAutenticacao.ts
@@ -3,6 +3,10 @@ import { verify } from 'jsonwebtoken';
 
 const { SECRET_KEY } = process.env
 
+interface Payload {
+    sub: string;
+}
+
 export function validarAutenticacao(req: Request, res: Response, next: NextFunction) {
     const autenticacao = req.headers.authorization
     if(!autenticacao) {
@@ -12,12 +16,16 @@ export function validarAutenticacao(req: Request, res: Response, next: NextFunct
     const [, token] = autenticacao.split(' ')
 
     if (!token) {
-        return res.status(401).json({ erro: "Token mal formatado!" });
+        // return res.status(401).json({ erro: "Token mal formatado!" });
+        return res.status(401).end()
     }
 
     try {
         // Verificar e decodificar o token
-        const decoded = verify(token, SECRET_KEY);
+        const { sub } = verify(token, SECRET_KEY) as Payload;
+
+        req.usuario_id = sub;
+        
         //req.user = decoded; // Se quiser adicionar informações do usuário no request
         return next();
     } catch (error) {
