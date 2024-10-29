e85a8c485f406f23c8d3cb13d1eabf2186c16dab
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-10-29 14:38:22 -0300

    Novas atualizações para realização de testes

--- dist/controllers/CargosControllers.js
+++ dist/controllers/CargosControllers.js
@@ -1,34 +1,32 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CargosControllers = void 0;
-const CargosServices_1 = require("../services/CargosServices");
-const services = new CargosServices_1.CargosServices();
-class Cargos {
-    async criar(req, res) {
-        const { cargo } = req.body;
-        const criar = await services.criarCargos(cargo);
-        return res.json(criar);
-    }
-    async listar(req, res) {
-        const cargos = await services.listarCargos();
-        return res.json(cargos);
-    }
-    async listarId(req, res) {
-        const { id } = req.params;
-        const cargo = await services.listarCargoID(parseInt(id));
-        return res.json(cargo);
-    }
-    async editar(req, res) {
-        const { id } = req.params;
-        const { cargo } = req.body;
-        const cargoId = await services.editarCargo(parseInt(id), cargo);
-        return res.json(cargoId);
-    }
-    async apagar(req, res) {
-        const { id } = req.params;
-        const apagar = await services.deletarCargo(parseInt(id));
-        return res.json(apagar);
-    }
-}
-const CargosControllers = new Cargos();
-exports.CargosControllers = CargosControllers;
+// import { Request, Response } from 'express';
+// import { CargosServices } from '../services/CargosServices';
+// const services = new CargosServices();
+// class Cargos {
+//     async criar(req: Request, res: Response) {
+//         const { cargo } = req.body;
+//         const criar = await services.criarCargos(cargo);
+//         return res.json(criar);
+//     }
+//     async listar(req: Request, res: Response) {
+//         const cargos = await services.listarCargos();
+//         return res.json(cargos);
+//     }
+//     async listarId(req: Request, res: Response) {
+//         const { id } = req.params;
+//         const cargo = await services.listarCargoID(parseInt(id));
+//         return res.json(cargo);
+//     }
+//     async editar(req: Request, res: Response) {
+//         const { id } = req.params;
+//         const { cargo } = req.body;
+//         const cargoId = await services.editarCargo(parseInt(id), cargo);
+//         return res.json(cargoId);
+//     }
+//     async apagar(req: Request, res: Response) {
+//         const { id } = req.params;
+//         const apagar = await services.deletarCargo(parseInt(id));
+//         return res.json(apagar);
+//     }
+// }
+// const CargosControllers = new Cargos();
+// export { CargosControllers };
