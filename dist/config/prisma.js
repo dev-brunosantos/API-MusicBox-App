b3d4a06710485b4e07980dd0f04417a1997922c8
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-10-29 15:43:03 -0300

    Desativando a compilação dos arquivos .ts

--- dist/config/prisma.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Prisma = void 0;
-const client_1 = require("@prisma/client");
-const Prisma = new client_1.PrismaClient();
-exports.Prisma = Prisma;
